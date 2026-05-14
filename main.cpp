1b6031e710fc6461044f900e46b3f4e62ac9164c
Stefan Shinkevich <stefan.shinkevich@gmail.com>
2026-05-14 16:06:57 +0200

    Fjerner unødvendig loggtekst fra ffplay

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <cstdlib>
 
 int main() {
-    system("ffplay -nodisp -autoexit http://lyd.nrk.no/nrk_radio_p1_stor-oslo_mp3_h");
+    system("ffplay -nodisp -hide_banner http://lyd.nrk.no/nrk_radio_p1_stor-oslo_mp3_h");
     return 0;
 }
