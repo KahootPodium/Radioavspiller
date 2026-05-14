26ba2553f30bf10798a1309ddba9cacff315cc36
Stefan Shinkevich <stefan.shinkevich@gmail.com>
2026-05-14 22:01:07 +0200

    Legger til flere kanaler

--- main.cpp
+++ main.cpp
@@ -15,6 +15,23 @@ int main() {
         "NRK P1",
         "NRK P2",
         "NRK P3",
+        "NRK Super",
+        "NRK Klassisk",
+        "NRK Jazz",
+        "NRK Folkemusikk",
+        "NRK Sport",
+        "P4 Norge",
+        "P5 Hits",
+        "P6 Rock",
+        "P7 Klem",
+        "P8 Pop",
+        "P9 Retro",
+        "P10 Country",
+        "P11 Dance",
+        "P12 Hitmix",
+        "Radio Norge",
+        "Radio Rock",
+        "Radio Kiss",
         "Quit"
     };
 
@@ -42,8 +59,8 @@ int main() {
         if (key == KEY_UP) choice--;
         if (key == KEY_DOWN) choice++;
 
-        if (choice < 0) choice = 3;
-        if (choice > 3) choice = 0;
+        if (choice < 0) choice = 20;
+        if (choice > 20) choice = 0;
 
         if (key == 10) break; // Avslutter menyen når enter trykkes
     }
@@ -62,6 +79,74 @@ int main() {
 
         case 2:
             playRadio("http://lyd.nrk.no/nrk_radio_p3_mp3_h");
+            break;
+        
+        case 3:
+            playRadio("http://lyd.nrk.no/nrk_radio_super_mp3_h");
+            break;
+
+        case 4:
+            playRadio("http://lyd.nrk.no/nrk_radio_klassisk_mp3_h");
+            break;
+        
+        case 5:
+            playRadio("http://lyd.nrk.no/nrk_radio_jazz_mp3_h");
+            break;
+
+        case 6:
+            playRadio("http://lyd.nrk.no/nrk_radio_folkemusikk_mp3_h");
+            break;
+
+        case 7:
+            playRadio("http://lyd.nrk.no/nrk_radio_sport_mp3_h");
+            break;
+
+        case 8:
+            playRadio("https://p4.p4groupaudio.com/P04_MH");
+            break;
+
+        case 9:
+            playRadio("https://p4.p4groupaudio.com/P05_MH");
+            break;
+
+        case 10:
+            playRadio("https://p4.p4groupaudio.com/P06_MH");
+            break;
+
+        case 11:
+            playRadio("https://p4.p4groupaudio.com/P07_MH");
+            break;
+
+        case 12:
+            playRadio("https://p4.p4groupaudio.com/P08_MH");
+            break;
+        
+        case 13:
+            playRadio("https://p4.p4groupaudio.com/P09_MH");
+            break;
+
+        case 14:
+            playRadio("https://p4.p4groupaudio.com/P10_MH");
+            break;
+
+        case 15:
+            playRadio("https://p4.p4groupaudio.com/P11_MH");
+            break;
+
+        case 16:
+            playRadio("https://p4.p4groupaudio.com/P12_MH");
+            break;
+
+        case 17:
+            playRadio("https://live-bauerno.sharp-stream.com/radionorge_no_mp3");
+            break;
+        
+        case 18:
+            playRadio("https://live-bauerno.sharp-stream.com/radiorock_no_mp3");
+            break;
+
+        case 19:
+            playRadio("https://live-bauerno.sharp-stream.com/kiss_no_mp3");
             break;
     }
 }
